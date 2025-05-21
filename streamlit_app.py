ef8260f01390c7caedde156a7b3d50720012efdf
svanhooser1 <108008281+svanhooser1@users.noreply.github.com>
2025-05-21 12:47:06 -0500

    Update streamlit_app.py
    Moved new request section from top to bottom

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,11 +2,6 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 
-# New section to display smoothiefroot nutrition information
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
-
 # Write directly to the app
 st.title(":cup_with_straw: Customize your Smoothie :cup_with_straw:")
 st.write(
@@ -46,3 +41,8 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         
         st.success('Your Smoothie is ordered!', icon="✅")
+
+# New section to display smoothiefroot nutrition information
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
